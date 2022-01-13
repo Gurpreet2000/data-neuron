66a9b7c700f9f14ec14467c56e0b19504e92ef84
gurpreet <singhgurpreet1212@gmail.com>
2022-01-13 07:09:45 +0530

    data neuron

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Split from 'react-split';
+import Content from './Content';
+
+const App = () => {
+	const splitProps = {
+		sizes: [50, 50],
+		minSize: 250,
+		expandToMin: false,
+		gutterSize: 10,
+		gutterAlign: 'center',
+	};
+	return (
+		<Split
+			{...splitProps}
+			direction="vertical"
+			cursor="row-resize"
+			direction="vertical"
+			className="split-row"
+		>
+			<Split
+				{...splitProps}
+				direction="horizontal"
+				cursor="col-resize"
+				className="split-col"
+			>
+				<Content contentId="a">A</Content>
+				<Content contentId="b">B</Content>
+			</Split>
+			<Content contentId="c">C</Content>
+		</Split>
+	);
+};
+
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import content from '../api/content';
+
+const Content = ({ children, contentId }) => {
+	const [isEditing, setIsEditing] = useState(false);
+	const [text, setText] = useState(children);
+	const [method, setMethod] = useState('get');
+	const [count, setCount] = useState(0);
+
+	useEffect(async () => {
+		const { data } = await content.get(`/${contentId}/count`);
+		setCount(data.count);
+	}, []);
+
+	return (
+		<div className="content">
+			{isEditing ? (
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<textarea value={text} onChange={e => setText(e.target.value)} />
+					<button
+						className="btn bg-success"
+						onClick={async () => {
+							const res = await content[method]('/', {
+								id: contentId,
+								data: text,
+								count,
+							});
+							setIsEditing(false);
+							setCount(count + 1);
+						}}
+					>
+						Submit
+					</button>
+				</div>
+			) : (
+				text
+			)}
+			<div>
+				<button
+					className="btn bg-primary"
+					onClick={() => {
+						setIsEditing(true);
+						setMethod('post');
+					}}
+					disabled={isEditing ? true : false}
+				>
+					Add
+				</button>
+				<button
+					className="btn bg-warning"
+					onClick={() => {
+						setIsEditing(true);
+						setMethod('put');
+					}}
+					disabled={isEditing ? true : false}
+				>
+					Update
+				</button>
+				<div style={{ textAlign: 'center' }}>Count: {count}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Content;
